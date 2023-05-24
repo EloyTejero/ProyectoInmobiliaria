5de65f247b66f0bc36860a5600e2801fb22e3ba7
kiotoks <nikogr2005@gmail.com>
2023-05-24 09:34:25 -0300

    Nicolas Garcia

--- Inmobiliaria.java
+++ Inmobiliaria.java
@@ -2,12 +2,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ProyectoInmobiliaria;
+package proyectoinmobiliaria;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author ET36
  */
 public class Inmobiliaria {
+    ArrayList<Propiedad> prop = new ArrayList<>;
+
+    public Inmobiliaria(ArrayList<Propiedad> al) {
+        this.prop = al;
+    }
     
+    public void agregarPropiedad(pro){
+        prop.add(pro);
+    }
+    
+    public Propiedad obtenerPropiedadID(int id){
+       for(ArrayList<Propiedad> p: prop){
+            if (p.getId() == id){
+                System.out.println("La propiedad que estas buscando es: " + p.toString());
+            }
+        }  
+    }
+
 }
